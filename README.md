cb735229b3e9558936f586f18edfd99b90e04a5e
Frank C Langbein <frank@langbein.org>
2022-09-03 13:13:29 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Info - SpinNet
+
+This is a mirror for long-term archival of the [Qyber\black](https://qyber.black)
+projects summary on [SpinNet](https://qyber.black/spinnet/info-spinnet).
+
+This is just the code to update the mnirror monthly. The mirrored data are in the [wiki](https://github.com/qyber-black/Info-SpinNet/wiki).
---
+++ mirror.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "${WIKI_PERSONAL_ACCESS_TOKEN}" ]; then
+  if [ -z "${GITHUB_PERSONAL_ACCESS_TOKEN}" ] ; then
+    echo "WIKI_PERSONAL_ACCESS_TOKEN not set"
+    exit 1
+  fi
+  WIKI_PERSONAL_ACCESS_TOKEN="${GITHUB_PERSONAL_ACCESS_TOKEN}"
+fi
+if [ -z "${GITHUB_SERVER_URL}" ]; then
+  GITHUB_SERVER_URL=https://github.com
+fi
+if [ -z "$GITHUB_REPOSITORY" ]; then
+  GITHUB_REPOSITORY="qyber-black/Info-SpinNet"
+fi
+
+echo
+echo "# Cloning github wiki"
+git clone "https://${WIKI_PERSONAL_ACCESS_TOKEN}@${GITHUB_SERVER_URL#https://}/$GITHUB_REPOSITORY.wiki.git"
+wiki_dir="`basename $GITHUB_REPOSITORY.wiki`"
+
+echo
+echo "# Sync'ing with qyber wiki"
+cd "$wiki_dir"
+git remote add qyber https://qyber.black/spinnet/info-spinnet.wiki.git
+git config --global user.email "frank@langbein.org"
+git config --global user.name "Frank C Langbein (via github actions)"
+git config pull.rebase true
+git fetch qyber master
+git merge --no-edit qyber/master
+if ! diff -q home.md Home.md; then
+  cp home.md Home.md
+  git add --all
+  git commit  -m "Update"
+fi
+
+echo
+echo "# Update wiki"
+git push
